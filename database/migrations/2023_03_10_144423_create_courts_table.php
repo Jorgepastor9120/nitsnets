87b6c7c4672394d215a7592e2c3251cc01a00bc6
jorgepastor9120 <jorgepastor9120@gmail.com>
2023-03-10 15:57:21 +0100

    Migraciones

---
+++ database/migrations/2023_03_10_144423_create_courts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courts', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('sport_id')
+                ->nullable();
+            $table->foreign('sport_id')
+                ->references('id')
+                ->on('sports')
+                ->onDelete('set null');
+            $table->string('name');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courts');
+    }
+};
---
+++ database/migrations/2023_03_10_145214_create_bookings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('member_id')
+                ->nullable();
+            $table->foreign('member_id')
+                ->references('id')
+                ->on('members')
+                ->onDelete('set null');
+            $table->unsignedBigInteger('court_id')
+                ->nullable();
+            $table->foreign('court_id')
+                ->references('id')
+                ->on('courts')
+                ->onDelete('set null');
+            $table->date('date');
+            $table->string('hour', 5);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
